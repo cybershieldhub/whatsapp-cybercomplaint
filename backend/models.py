c6477522cb5252e7428fbcfab5dcf37148be0812
M1 <imagelocker16@google.com>
2025-10-12 21:45:20 +0530

    Add M1 backend folder structure with starter files

---
+++ backend/models.py
@@ -0,0 +1,9 @@
+﻿from sqlalchemy import Column, Integer, String
+from db import Base
+
+class Complaint(Base):
+    __tablename__ = 'complaints'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    email = Column(String)
+    details = Column(String)
---
+++ backend/schemas.py
@@ -0,0 +1,15 @@
+﻿from pydantic import BaseModel
+
+class ComplaintBase(BaseModel):
+    name: str
+    email: str
+    details: str
+
+class ComplaintCreate(ComplaintBase):
+    pass
+
+class Complaint(ComplaintBase):
+    id: int
+
+    class Config:
+        orm_mode = True
